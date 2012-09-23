9246fddc269e4722e18ef5a0064d4ee65d24ab61
Michael Peacock <mkpeacock@gmail.com>
2012-09-23 15:27:50 +0100

    More usage

--- src/usage.php
+++ src/usage.php
@@ -9,9 +9,19 @@ $classLoader->register();
 $classLoader = new SplClassLoader('CentralApps\PostMarkApp', __DIR__ . '/' );
 $classLoader->register();
 
+$sender = new \CentralApps\Mail\SendersReceiversEtc\Sender("mkpeacock@gmail.com", "Michael Peacock");
+
 $configuration = new \CentralApps\PostMarkApp\Configuration();
 $message = new \CentralApps\PostMarkApp\Message();
 $transport = new \CentralApps\PostMarkApp\Transport( $configuration );
 
 $dispatcher = new \CentralApps\Mail\Dispatcher();
+
+$message->setSender($sender);
+$message->setPlainTextMessage("Hi there");
+
+$recipient = new \CentralApps\Mail\SendersReceiversEtc\Recipient("mkpeacock@gmail.com", "Michael Peacock");
+$message->addRecipient($recipient);
+
+
 
