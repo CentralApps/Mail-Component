5507925be00a2a1afc4817b0a787072c142d212d
Michael Peacock <mkpeacock@gmail.com>
2012-09-23 16:10:48 +0100

    More usage

--- src/usage.php
+++ src/usage.php
@@ -9,13 +9,14 @@ $classLoader->register();
 $classLoader = new SplClassLoader('CentralApps\PostMarkApp', __DIR__ . '/' );
 $classLoader->register();
 
-$sender = new \CentralApps\Mail\SendersReceiversEtc\Sender("mkpeacock@gmail.com", "Michael Peacock");
+$sender = new \CentralApps\Mail\SendersReceiversEtc\Sender("michael@peacocknet.co.uk", "Michael Peacock");
 
 $configuration = new \CentralApps\PostMarkApp\Configuration();
+$configuration['api_key'] = "";
 $message = new \CentralApps\PostMarkApp\Message();
 $transport = new \CentralApps\PostMarkApp\Transport( $configuration );
 
-$dispatcher = new \CentralApps\Mail\Dispatcher();
+$dispatcher = new \CentralApps\Mail\Dispatcher($transport);
 
 $message->setSender($sender);
 $message->setPlainTextMessage("Hi there");
@@ -23,5 +24,6 @@ $message->setPlainTextMessage("Hi there");
 $recipient = new \CentralApps\Mail\SendersReceiversEtc\Recipient("mkpeacock@gmail.com", "Michael Peacock");
 $message->addRecipient($recipient);
 
+$dispatcher->send($message);
 
 
